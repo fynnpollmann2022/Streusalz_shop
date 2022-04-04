05b26a20a205afde9712a7e8ce58077a953e73f3
Shopmacher <shopmacher@mbpvonsopmacher.shopmacher.de>
2022-04-04 16:32:09 +0200

    Feedback Formular

---
+++ php/feedback.php
@@ -0,0 +1,19 @@
+<?php
+include 'Twig.php';
+session_start();
+
+if(!isset($_SESSION["username"])){
+    header("Location: index.php");
+    exit;
+}
+
+if(isset($_POST["submit"])) {
+    mail("pollmannfynn@gmail.com", "Kontaktformular", 'Name: ' . $_SESSION["username"] . ' Email: ' . $_SESSION['email'] . ' Sterne: ' . $_POST["sterne"] . ' Nachricht: ' . $_POST["message"]);
+}
+echo ('Feedback wurde abgeschickt!');
+
+echo Twig()->render('feedback.html.twig',[
+                     'sterne' => $_POST["sterne"],
+                     'message' => $_POST["message"],
+                     'username' => $_SESSION['username'],
+                    'email' => $_SESSION['email'],]);
